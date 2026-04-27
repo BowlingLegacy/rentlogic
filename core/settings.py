fb54c16bda8440e4b0abdb5e26227fef665ea342
BowlingLegacy <bowlinglegacyllc@outlook.com>
2026-04-27 14:08:51 -0700

    Fix STATIC_ROOT for Render

--- core/settings.py
+++ core/settings.py
@@ -114,4 +114,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'static/'git add .
+git commit -m "Fix STATIC_ROOT for Render"
+git push
