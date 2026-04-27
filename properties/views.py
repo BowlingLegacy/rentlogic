f686839d1e34a214a1f1b0770a3ad356308903cb
BowlingLegacy <bowlinglegacyllc@outlook.com>
2026-04-27 14:49:35 -0700

    Enable templates folder

--- properties/views.py
+++ properties/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("RentLogic is live 🚀")
+    return render(request, "core/home.html")
